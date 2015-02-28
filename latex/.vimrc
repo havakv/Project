0c679b3275921c7b36c222683f55950f19b03039
havakv <haavard.kvamme@gmail.com>
2015-02-28 17:06:18 +0100

    Wrote compiler that delete cached stuff

--- latex/.vimrc
+++ latex/.vimrc
@@ -1,1 +1,1 @@
-au FileType rnoweb,tex map <F12> :! ./compile_Rnw.sh
+au FileType rnoweb,tex map <F12> :! ./compile.sh
--- latex/compile/compile_Rnw.sh
+++ latex/compile/compile_Rnw.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 # Make .tex file
-#name=$(echo $1 | cut -d "." -f1)
-name="project"
+name=$(echo $1 | cut -d "." -f1)
 echo -e "require('knitr') \n knit('${name}.Rnw')" | R --no-save -q
 
 # Compile to pdf
