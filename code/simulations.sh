72c2bbef2e91936a3b1156a92216b84af7d1f0e0
havakv <haavard.kvamme@gmail.com>
2015-04-27 17:34:24 +0200

    Correct naming

--- code/simulations.sh
+++ code/simulations.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-OMP_NUM_THREADS=1 nice ./adaboostSim.R 10
+OMP_NUM_THREADS=1 nice ./adaboostSpam.R 10
 
-OMP_NUM_THREADS=1 nice ./gradBoost.R 5
+OMP_NUM_THREADS=1 nice ./gradBoostSpam.R 5
 
 nice ./baggingSpam.R
 
