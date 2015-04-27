6e3e54c040d1c786a431c7b1e0ee8b0906b3f9e6
havakv <haavard.kvamme@gmail.com>
2015-04-27 11:03:45 +0200

    Adaboost simulations and plot

---
+++ code/adaboostPlot.R
@@ -0,0 +1,15 @@
+#!/usr/bin/env Rscript
+# Plotting for adaboost on spam data
+    
+
+load("../latex/dataR/adaboostSim.Rdata")
+
+# Plot figures
+printfig("adaboost", NOPRINT)
+ylim <- c(min(Errors), max(Errors))
+plot(its, Errors[,1], type="l", xlab = "iterations", ylab = "error", ylim = ylim)
+for (j in 2:ndept) {
+    lines(its, Errors[, j], col = j)
+}
+legend(x = "topright", as.character(maxdepth), lty = rep(1, 4), lwd = rep(1, 4), col = 1:ndept, bg="white")
+off(NOPRINT)
--- code/adaboostSim.R
+++ code/adaboostSim.R
@@ -44,17 +44,6 @@ Errors <- foreach(i = 1:nit, .combine = rbind) %dopar% {
     err
 }
 
-# Plot figures
-printfig("adaboost", NOPRINT)
-ylim <- c(min(Errors), max(Errors))
-plot(its, Errors[,1], type="l", xlab = "iterations", ylab = "error", ylim = ylim)
-for (j in 2:ndept) {
-    lines(its, Errors[, j], col = j)
-}
-legend(x = "topright", as.character(maxdepth), lty = rep(1, 4), lwd = rep(1, 4), col = 1:ndept, bg="white")
-off(NOPRINT)
-
-
 # Save variables
 save(maxdepth, its, ndept, nit, Errors, file = "../latex/dataR/adaboostSim.Rdata")
 
