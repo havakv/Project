74103da0151eaf6d56b10d287fad6af2cc32bfd5
Håvard Kvamme <havakv@banach01.math.ntnu.no>
2015-05-22 18:43:41 +0200

    more simulations

---
+++ code/spam/OOBvsCVvsTestPlot.R
@@ -0,0 +1,14 @@
+#!/usr/bin/env Rscript
+
+NOPRINT <- FALSE
+library(common)
+    
+load("../../dataset/spamResults/OOBvsCVvsTest.Rdata")
+
+printfig("OOBvsTestvsCV", NOPRINT)
+ylim <- c(min(c(errVec, fit$err.rate[,1])), 0.08)
+plot(index, fit$err.rate[,1][index], ylab = "error", xlab = "nr. trees", type = "l", ylim = ylim, col = 3)
+lines(index, errVec, col = 4)
+lines(index, rowMeans(Rates), col = 2)
+grid()
+legend(x = "topright", c("OOB", "Test", "CV"), lty = rep(1, 3), lwd = rep(2, 3), col = c(3, 4, 2), bg="white")
---
+++ code/spam/RFDepthSpam.R
@@ -0,0 +1,53 @@
+#!/usr/bin/env Rscript
+# Different tree depths of random forest
+
+# Check if environment variables are fine.
+if(system("echo $OMP_NUM_THREADS", intern = TRUE) != 1)
+    stop("OMP_NUM_THREADS is not 1")
+
+argv <- commandArgs(trailingOnly=TRUE)
+if (identical(argv, character(0)))
+    stop("Need number of threads as input parameter")
+
+nCores <- as.integer(argv[1])
+
+set.seed(0)
+library(common)
+library(randomForest)
+require(parallel) # one of the core R packages
+require(doParallel)
+library(foreach)
+library(cvTools)
+
+X <- getSpam()
+
+nodesize <- c(1, 5, 10, 40)
+nsizes <- length(nodesize)
+nTree <- 600
+nindex <- 100
+index <- round(seq(1, nTree, length.out=nindex))
+
+registerDoParallel(nCores)
+Errors <- matrix(NA, nindex, nsizes)
+
+seeds <- sample.int(1e7, nsizes)
+Errors <- foreach(i = 1:nsizes, .combine = cbind) %dopar% {
+    cat("Starting ", i, "\n", sep = '')
+    set.seed(seeds[i])
+
+    fit <- randomForest(spam ~ ., data = X$train, ntree = nTree, nodesize = nodesize[i])
+    err <- rep(NA, nindex)
+    for (j in 1:nindex) {
+        fit$forest$ntree <- index[j]
+        pred <- predict(fit, X$test, type="response")
+        err[j] <- sum(pred != X$test$spam)/X$nTest
+    }
+     cat("Ending ", i, "\n", sep = '')
+    err
+}
+
+
+# Save variables
+save(Errors, index, nodesize, nsizes, file = "../../dataset/spamResults/RFDepthSpam.Rdata")
+
+
